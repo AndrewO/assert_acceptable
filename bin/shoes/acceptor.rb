3d81c05995a05e1f21b498691077c2e92fd3cba4
Andrew O'Brien <andrew@econify.com>
2010-04-07 21:35:15 -0400

    Added hot keys

--- bin/shoes/acceptor.rb
+++ bin/shoes/acceptor.rb
@@ -2,13 +2,32 @@ Shoes.app do
   resource_path = ARGV[1]
   assertion = ARGV[2].to_s.empty? ? "look right" : ARGV[2].to_s
 
+  keypress do |key|
+    case key
+    when "\n"
+      pass
+    when "\e"
+      fail
+    end
+  end
+
   stack :margin => 10 do
     para "Does this image ", assertion, "?"
     flow do
-      button("Yes") { puts "1"; exit }
-      button("No") { puts "0"; exit }
+      button("Yes") { pass }
+      button("No") { fail }
     end
     image resource_path
     para code(resource_path)
+  end
+  
+  def pass
+    puts "1"
+    exit
+  end
+  
+  def fail
+    puts "0"
+    exit
   end
 end
